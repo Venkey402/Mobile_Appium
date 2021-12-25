322b47c2add48724a206ddb5ad96aa86f57d71d4
venkey402 <venkey402@gmail.com>
2021-12-25 18:52:19 +0530

    Added making a call functionality

--- src/test/java/com/Appium/PageObjects/Dialer_HomePage.java
+++ src/test/java/com/Appium/PageObjects/Dialer_HomePage.java
@@ -17,7 +17,7 @@ public class Dialer_HomePage {
 		PageFactory.initElements(driver, this);
 	}
 	//************************ Locator Strategies *********************************
-	//1. Digits
+	//1. Tabs
 	@FindBy(how=How.ID,using="com.android.dialer:id/contacts_tab")
 	WebElement ContactsTab;
 	@FindBy(how=How.ID,using="com.android.dialer:id/speed_dial_tab")
@@ -29,10 +29,38 @@ public class Dialer_HomePage {
 	@FindBy(how=How.ID,using="com.android.dialer:id/empty_list_view_action")
 	WebElement btnContactsEmptyList;
 	@FindBy(how=How.ID,using="com.android.contacts:id/right_button")
-	WebElement btnAddContacts;
+	WebElement btnAddContacts;	
+	@FindBy(how=How.ID, using ="com.android.dialer:id/fab")
+	WebElement icnDialerPad;
+	@FindBy(how=How.ID, using ="com.android.dialer:id/dialpad_floating_action_button")
+	WebElement icnDial;
+	@FindBy(how=How.ID, using ="com.android.dialer:id/incall_end_call")
+	WebElement btnEndCall;
 	
 	
 	
+	
+	//2. Numbers
+	@FindBy(how=How.ID,using="com.android.dialer:id/zero")
+	WebElement Num_Zero;
+	@FindBy(how=How.ID,using="com.android.dialer:id/one")
+	WebElement Num_One;	
+	@FindBy(how=How.ID,using="com.android.dialer:id/two")
+	WebElement Num_Two;
+	@FindBy(how=How.ID,using="com.android.dialer:id/three")
+	WebElement Num_Three;
+	@FindBy(how=How.ID,using="com.android.dialer:id/four")
+	WebElement Num_Four;
+	@FindBy(how=How.ID,using="com.android.dialer:id/five")
+	WebElement Num_Five;
+	@FindBy(how=How.ID,using="com.android.dialer:id/six")
+	WebElement Num_Six;	
+	@FindBy(how=How.ID,using="com.android.dialer:id/seven")
+	WebElement Num_Seven;	
+	@FindBy(how=How.ID,using="com.android.dialer:id/eight")
+	WebElement Num_Eight;
+	@FindBy(how=How.ID,using="com.android.dialer:id/nine")
+	WebElement Num_Nine;
 	
 	
 	
@@ -64,4 +92,59 @@ public class Dialer_HomePage {
 	{
 		btnContactsEmptyList.click();
 	}
+	public void ClickDialPad()
+	{
+		icnDialerPad.click();
+	}
+	public void ClickDialerIcon()
+	{
+		icnDial.click();
+	}
+	public void btnEndCall()
+	{
+		icnDial.click();
+	}
+	
+	// 2. Digits
+	
+		public void PressOne()
+		{
+			Num_One.click();
+		}	
+		public void PressTwo()
+		{
+			Num_Two.click();
+		}
+		public void PressThree()
+		{
+			Num_Three.click();
+		}	
+		public void PressFour()
+		{
+			Num_Four.click();
+		}
+		public void PressFive()
+		{
+			Num_Five.click();
+		}	
+		public void PressSix()
+		{
+			Num_Six.click();
+		}
+		public void PressSeven()
+		{
+			Num_Seven.click();
+		}	
+		public void PressEight()
+		{
+			Num_Eight.click();
+		}
+		public void PressNine()
+		{
+			Num_Nine.click();
+		}	
+		public void PressZero()
+		{
+			Num_Zero.click();
+		}
 }
--- src/test/java/com/Appium/Testcases/TC_Dialer_002.java
+++ src/test/java/com/Appium/Testcases/TC_Dialer_002.java
@@ -12,10 +12,20 @@ public class TC_Dialer_002 extends BaseClass{
 		Dialer_HomePage dhp = new Dialer_HomePage(driver);		
 		dhp.ClickContacts_Tab();
 		Thread.sleep(5000);
-		dhp.ClickCreateNewContact();
+		dhp.ClickDialPad();
 		Thread.sleep(5000);
-		dhp.ClickAddNewContact();
-		Thread.sleep(5000);
+		dhp.PressNine();
+		dhp.PressZero();
+		dhp.PressThree();
+		dhp.PressZero();
+		dhp.PressOne();
+		dhp.PressNine();
+		dhp.PressSix();
+		dhp.PressNine();
+		dhp.PressZero();
+		dhp.PressSeven();
+		dhp.ClickDialerIcon();
+		dhp.btnEndCall();		
 	}
 	
 
