bde780d2dd5e50133591226773c099a0a933d659
venkey402 <venkey402@gmail.com>
2021-12-25 11:20:10 +0530

    Added locator strategies and action methods for the remaning digits.

--- src/test/java/com/Appium/PageObjects/HomePage.java
+++ src/test/java/com/Appium/PageObjects/HomePage.java
@@ -16,35 +16,105 @@ public class HomePage {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
+	//************************ Locator Strategies *********************************
+	//1. Digits
+	@FindBy(how=How.ID,using="com.google.android.calculator:id/digit_0")
+	WebElement Num_Zero;
+	@FindBy(how=How.ID,using="com.google.android.calculator:id/digit_1")
+	WebElement Num_One;	
+	@FindBy(how=How.ID,using="com.google.android.calculator:id/digit_2")
+	WebElement Num_Two;
+	@FindBy(how=How.ID,using="com.google.android.calculator:id/digit_3")
+	WebElement Num_Three;
+	@FindBy(how=How.ID,using="com.google.android.calculator:id/digit_4")
+	WebElement Num_Four;
+	@FindBy(how=How.ID,using="com.google.android.calculator:id/digit_5")
+	WebElement Num_Five;
+	@FindBy(how=How.ID,using="com.google.android.calculator:id/digit_6")
+	WebElement Num_Six;	
 	@FindBy(how=How.ID,using="com.google.android.calculator:id/digit_7")
-	WebElement Num_Seven;
-	@FindBy(how=How.ID,using="com.google.android.calculator:id/op_add")
-	WebElement Num_Add;
+	WebElement Num_Seven;	
 	@FindBy(how=How.ID,using="com.google.android.calculator:id/digit_8")
 	WebElement Num_Eight;
+	@FindBy(how=How.ID,using="com.google.android.calculator:id/digit_9")
+	WebElement Num_Nine;	
+	
+	//2. Operators
 	@FindBy(how=How.ID,using="com.google.android.calculator:id/eq")
 	WebElement Num_EqualsTo;
+	@FindBy(how=How.ID,using="com.google.android.calculator:id/op_add")
+	WebElement Num_Add;
+	@FindBy(how=How.ID,using="com.google.android.calculator:id/op_sub")
+	WebElement Num_Substract;
+	@FindBy(how=How.ID,using="com.google.android.calculator:id/op_mul")
+	WebElement Num_Multiply;
+	@FindBy(how=How.ID,using="com.google.android.calculator:id/op_div")
+	WebElement Num_Division;
 	
 	
-	// Action methods 
-	
+	// ************************ Action methods *********************************
+	// 1. Digits
+	public void PressOne()
+	{
+		Num_One.click();
+	}	
+	public void PressTwo()
+	{
+		Num_Two.click();
+	}
+	public void PressThree()
+	{
+		Num_Three.click();
+	}	
+	public void PressFour()
+	{
+		Num_Four.click();
+	}
+	public void PressFive()
+	{
+		Num_Five.click();
+	}	
+	public void PressSix()
+	{
+		Num_Six.click();
+	}
 	public void PressSeven()
 	{
 		Num_Seven.click();
-	}
-	public void PressAdd()
-	{
-		Num_Add.click();
-	}
+	}	
 	public void PressEight()
 	{
 		Num_Eight.click();
 	}
-	public void PressEqualsTo()
+	public void PressNine()
 	{
-		Num_EqualsTo.click();
+		Num_Nine.click();
+	}	
+	public void PressZero()
+	{
+		Num_Zero.click();
 	}
 	
 	
+	//2. Operations
+	public void PressAdd()
+	{
+		Num_Add.click();
+	}
+	public void PressSubstract()
+	{
+		Num_Substract.click();
+	}
+	public void PressMultiply()
+	{
+		Num_Multiply.click();
+	}
+	public void PressDividision()
+	{
+		Num_Division.click();
+	}
+	public void PressEqualsTo()
+	{
+		Num_EqualsTo.click();
+	}		
 }
--- src/test/java/com/Appium/Testcases/TC_Calculator_001.java
+++ src/test/java/com/Appium/Testcases/TC_Calculator_001.java
@@ -10,7 +10,7 @@ import junit.framework.Assert;
 public class TC_Calculator_001 extends BaseClass{
 	
 	@Test
-	public void Add_Calculator() throws InterruptedException{
+	public void ValidateAdditionOf7and8() throws InterruptedException{
 		
 		HomePage hp = new HomePage(driver);	
 		String Expected_Sum="15";		
@@ -19,6 +19,29 @@ public class TC_Calculator_001 extends BaseClass{
 		logger.info("Pressing 7 in the calculator..");
 		hp.PressAdd();
 		logger.info("Pressing + in the calculator..");
+		hp.PressEight();
+		logger.info("Pressing 8 in the calculator..");
+		hp.PressEqualsTo();
+		logger.info("Pressing = in the calculator..");
+		
+		WebElement Num_Result=driver.findElement(By.id("com.google.android.calculator:id/result_final"));
+		String Actual_Sum = Num_Result.getText();		
+		System.out.println("Total is : "+Actual_Sum);					
+		Thread.sleep(5000);		
+		Assert.assertEquals(Expected_Sum, Actual_Sum);		
+		System.out.println("Completed");	
+		takescreenshot();
+	}
+	@Test
+	public void ValidateMultiplicationOf9and8() throws InterruptedException{
+		
+		HomePage hp = new HomePage(driver);	
+		String Expected_Sum="72";		
+		
+		hp.PressNine();
+		logger.info("Pressing 9 in the calculator..");
+		hp.PressAdd();
+		logger.info("Pressing * in the calculator..");
 		hp.PressEight();
 		logger.info("Pressing 8 in the calculator..");
 		hp.PressEqualsTo();
